73caafbb62ab90c72cfe222a81fc87c553f82d97
Jack Polgar <nrx@nirix.net>
2012-06-10 12:23:34 +1000

    Allow setting of custom port: PORT=1234 node app.js

--- app.js
+++ app.js
@@ -80,6 +80,6 @@ app.get('/logout', routes.users.logout);
 app.get('/register', routes.users.register);
 app.post('/register', routes.users.create);
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
